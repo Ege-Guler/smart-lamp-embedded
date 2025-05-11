2f7adbb822b17f62c81272168209d7900d67d623
i1nac <egeguler2003@gmail.com>
2025-05-12 00:19:34 +0300

    Reapply "feature added captive portal"
    This reverts commit d7727a8e83ef038887774b9b389c9b12916116a1.

--- esp/esp.ino
+++ esp/esp.ino
@@ -6,6 +6,7 @@
 #include <PubSubClient.h>
 #include <WiFiClientSecure.h>
 #include <ArduinoJson.h>
+#include <DNSServer.h>
 
 #include "EEPROMHelper.h"
 
@@ -18,9 +19,13 @@
 #define WIFI_TIMEOUT 16000
 #define RESET_BUTTON_PIN 0
 
+const byte DNS_PORT = 53;
+
 // Acces Point Configuration
 const char *ssidAP = "h2-smart-lamp";
 const char *passwordAP = "configureme";
+
+IPAddress apIP(172, 217, 28, 1);
 
 bool resetTriggered = false;
 
@@ -47,6 +52,8 @@ Ticker blinker;
 WiFiClientSecure espClient;
 PubSubClient client(espClient);
 
+DNSServer dnsServer;
+
 static BearSSL::X509List *globalRootCert = nullptr;
 
 struct WifiConfig config;
@@ -72,9 +79,11 @@ void reconnect();
 void readMQTTConfig();
 bool syncNTP();
 
-void readMQTTConfig(){
+void readMQTTConfig()
+{
   File file = LittleFS.open("/mqtt.json", "r");
-  if(!file){
+  if (!file)
+  {
     Serial.println("Failed to open mqtt.json");
     return;
   }
@@ -83,7 +92,8 @@ void readMQTTConfig(){
   DeserializationError error = deserializeJson(doc, file);
   file.close();
 
-  if (error) {
+  if (error)
+  {
     Serial.println("Failed to parse config file");
     return;
   }
@@ -93,7 +103,6 @@ void readMQTTConfig(){
   mqttConfig.mqtt_pass = doc["mqtt_password"].as<String>();
 
   Serial.println("MQTT Config loaded successfully.");
-
 }
 
 bool loadRootCAFromFS()
@@ -251,7 +260,8 @@ void setMode(String mode)
 }
 
 // true STA, false AP
-bool loadMode(){
+bool loadMode()
+{
   bool mode;
   eepromRead(MOD_ADDR, mode);
   return mode;
@@ -292,7 +302,9 @@ String getContentType(String path)
 
 void startAP()
 {
+  WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
   WiFi.softAP(ssidAP, passwordAP);
+  dnsServer.start(DNS_PORT, "*", apIP);
   blinker.attach(0.5, blink);
 
   Serial.println("AP started. IP: " + WiFi.softAPIP().toString());
@@ -300,13 +312,27 @@ void startAP()
   server.on("/submit", handleFormSubmit);
 
   server.onNotFound([]()
-                    { handleFileRequest(server.uri()); });
+                    {
+  String path = server.uri();
+  if (LittleFS.exists(path)) {
+    handleFileRequest(path);
+  } else {
+    if (LittleFS.exists("/index.html")) {
+      handleFileRequest("/index.html");
+    } else {
+      server.send(200, "text/html", "<html><body><h1>Welcome</h1><p>Captive portal fallback page.</p></body></html>");
+    }
+  } });
+
+  // server.onNotFound([]()
+  //                   { handleFileRequest(server.uri()); });
 
   server.begin();
   Serial.println("HTTP server started (AP mode only)");
 }
 
-bool syncNTP(){
+bool syncNTP()
+{
   configTime(0, 0, "pool.ntp.org", "time.nist.gov"); // UTC, no DST, NTP servers
   Serial.print("Waiting for NTP time sync... ");
   time_t now = time(nullptr);
@@ -325,7 +351,6 @@ bool syncNTP(){
   Serial.print(asctime(&timeinfo));
   return true;
 }
-
 
 void connectionLoop()
 {
@@ -346,8 +371,9 @@ void connectionLoop()
   if (WiFi.status() == WL_CONNECTED)
   {
     Serial.println("\nConnected! IP: " + WiFi.localIP().toString());
-    
-    if(!syncNTP()){
+
+    if (!syncNTP())
+    {
       Serial.println("NTP sync failed.");
       return;
     }
@@ -442,6 +468,7 @@ void setup()
 void loop()
 {
 
+  dnsServer.processNextRequest();
   server.handleClient();
 
   if (isSTA)
