1e7a4266d1a38850309475f21aa8fc2dec7410f5
onuralpgunduz <onuralpgunduz@gmail.com>
2025-05-13 16:33:15 +0300

    added bar chart componenet

--- smart-home-ui/src/app/components/bar-graph/bar-graph.component.ts
+++ smart-home-ui/src/app/components/bar-graph/bar-graph.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ChartData, ChartOptions, CategoryScale, BarElement, LinearScale, BarController, Title, Chart } from 'chart.js'; 
+import { BaseChartDirective } from 'ng2-charts'; 
+import { CommonModule } from '@angular/common'; 
 @Component({
   selector: 'app-bar-graph',
-  standalone: true,
-  imports: [],
+  standalone: true, 
+  imports: [CommonModule, BaseChartDirective], 
   templateUrl: './bar-graph.component.html',
-  styleUrl: './bar-graph.component.scss'
+  styleUrls: ['./bar-graph.component.scss'],
 })
-export class BarGraphComponent {
+export class BarGraphComponent implements OnInit {
+  ngOnInit(): void {
+    Chart.register(CategoryScale, BarElement, LinearScale, BarController, Title);
+  }
 
+  public barChartData: ChartData<'bar'> = {
+    labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'], 
+    datasets: [
+      {
+        data: [7, 4, 6, 7, 3, 2, 1], // Example data
+        backgroundColor: [
+          '#FF4D4F', '#FACC15', '#00C58E', '#3B82F6', '#FFB020', '#FF3D71', '#F9FAFB', // Colors
+        ],
+        borderRadius: 5,
+      },
+    ],
+  };
+
+  public barChartOptions: ChartOptions<'bar'> = {
+    responsive: true,
+    scales: {
+      x: {
+        type: 'category',
+        min: 0,  // Ensure that the x-axis starts from 0
+      },
+      y: {
+        type: 'linear',  // Explicitly set the y-axis as linear scale
+        min: 0,  // Ensure that the y-axis starts from 0
+      },
+    },
+  };
 }
